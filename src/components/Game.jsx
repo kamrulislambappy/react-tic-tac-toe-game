db2b4dc9649c3eec0b678bc3dea04a2cb669c31b
KAMRULISLAMBAPPY <m.kamrulislambappy@gmail.com>
2025-01-18 22:13:45 +0600

    Tic Tac Toe Game via React

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -31,13 +31,11 @@ export default function Game() {
     }
 
     return (
-      <li key={move}>
-        <button
-          className="hover:text-gray-400 duration-200"
-          onClick={() => jumpHistory(move)}
-        >
-          {description}
-        </button>
+      <li
+        key={move}
+        className="py-[2px] rounded-md bg-yellow-200 hover:bg-yellow-300 duration-300"
+      >
+        <button onClick={() => jumpHistory(move)}>{description}</button>
       </li>
     );
   });
@@ -53,10 +51,10 @@ export default function Game() {
           />
         </div>
         <div className="w-44 text-center rounded-lg bg-stone-700">
-          <div className="pt-5 pb-3 text-yellow-200 font-semibold">
+          <div className="pt-4 pb-3 text-yellow-200 font-semibold">
             Game History
           </div>
-          <ul className="text-gray-300 space-y-3 text-sm">{moves}</ul>
+          <ul className="text-gray-800 space-y-3 text-sm px-4">{moves}</ul>
         </div>
       </div>
     </div>
